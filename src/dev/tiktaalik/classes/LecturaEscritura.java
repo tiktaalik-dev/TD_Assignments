d187331674387919ce6a3ac8b08915e1648b3c17
Rigo <rodrigo@tiktaalik.dev>
2024-08-11 08:51:06 -0400

    Finished the code for the LecturaEscritura.java class, from the Java section of the Android development course of the Talento Digital programme.

--- src/dev/tiktaalik/classes/LecturaEscritura.java
+++ src/dev/tiktaalik/classes/LecturaEscritura.java
@@ -5,10 +5,17 @@ import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public class LecturaEscritura {
-    private static String[] crearArchivo(String directorio, String fichero) throws IOException, SecurityException {
+    // Crear variables de clase
+    static String user_directory = System.getProperty("user.dir");
+    static String separator = File.separator;
+    static String src_directory = "%s%ssrc%s".formatted(user_directory, separator, separator);
+    static String new_line = System.lineSeparator();
+    static String error_msg;
+    static String exc_cause;
+
+    private static String[] crearArchivo(String directorio, String fichero) {
         // Crear variables de trabajo y respuesta
-        String separator = File.separator;
-        String folder_path = "src%s%s".formatted(separator, directorio);
+        String folder_path = "%s%s".formatted(src_directory, directorio);
         String file_path = "%s%s%s".formatted(folder_path, separator, fichero);
         File folder = new File(folder_path);
         File file = new File(file_path);
@@ -29,44 +36,48 @@ public class LecturaEscritura {
         String file_error_msg = base_error_msg.formatted(file_str);
         String file_exists_error_msg = base_exists_error_msg.formatted(file_str);
 
+        // Crear valores por defecto para la respuesta
+        answer[0] = "true";
+        answer[1] = "";
 
-        // Comprobar si ya existe la carpeta. Si existe, saltar este bloque. Caso contrario, crearlo
-        String userDirectory = System.getProperty("user.dir");
-        System.out.printf("Current user.dir is: %s%n", userDirectory);
+        // Comprobar si ya existe la carpeta. Si existe, saltar este bloque y guardar un mensaje de error para el
+        // usuario. Caso contrario, crearlo
         if (!folder.exists()) {
             try {
                 folder_created = folder.mkdirs();
             } catch (SecurityException folder_exc) {
                 // Incluir en la respuesta si es que hubo un mensaje de error
-                answer[1] = String.valueOf(folder_exc.getCause());
-                throw new RuntimeException(folder_exc);
+                exc_cause = String.valueOf(folder_exc.getCause());
+                answer[1] = "%s.%s%s".formatted(folder_error_msg, new_line, exc_cause);
             }
         } else answer[1] = folder_exists_error_msg;
 
-        // Comprobar si ya existe el archivo. Si existe, saltar este bloque. Caso contrario, crearlo
+        // Comprobar si ya existe el archivo. Si existe, saltar este bloque y guardar un mensaje de error para el
+        // usuario. Caso contrario, crearlo
         if (!file.exists()) {
             try {
                     file_created = file.createNewFile();
             } catch (SecurityException | IOException file_exc) {
                 // Incluir en la respuesta si es que hubo un mensaje de error
-                answer[1] = "%s.\n%s".formatted(answer[1], String.valueOf(file_exc.getCause()));
-                throw new RuntimeException(file_exc);
+                exc_cause = String.valueOf(file_exc.getCause());
+                answer[1] = "%s.%s%s%s%s".formatted(answer[1], new_line, file_error_msg, new_line, exc_cause);
             }
-        } else answer[1] = "%s%s".formatted(answer[1], file_exists_error_msg);
+        } else answer[1] = "%s.%s%s".formatted(answer[1], new_line, file_exists_error_msg);
 
-        // Construir la respuesta. Esta debe incluir si la operación fue exitosa y si hubo algún mensaje de error
-        answer[0] = "%s".formatted((folder_created && file_created) ? folder_success_msg : folder_error_msg);
+        // Construir la respuesta. Esta debe incluir si la operación fue exitosa en el primer elemento del array.
+        // Si hubo algún mensaje de error, este va en el segundo elemento del array
+        answer[0] = "%s".formatted((folder_created && file_created));
+        answer[1] = answer[0].equals("true") ? "%s%s%s".formatted(folder_success_msg, new_line, file_success_msg) : answer[1];
 
         // Retornar la respuesta
         return answer;
     }
 
 
-    private static String[] writeBuffer(String directory, String file_name, ArrayList<String> content) throws IOException {
+    private static String[] writeBuffer(String directory, String file_name, ArrayList<String> content) {
 
         // Definir variables de trabajo y respuesta
-        String separator = File.separator;
-        String folder_path = "src%s%s".formatted(separator, directory);
+        String folder_path = "%s%s".formatted(src_directory, directory);
         String file_path = "%s%s%s".formatted(folder_path, separator, file_name);
         File current_file = new File(file_path);
         FileWriter file_writer = null;
@@ -74,80 +85,89 @@ public class LecturaEscritura {
         Iterator<String> content_list = content.iterator();
         String[] answer = new String[2];
 
+        // Crear mensajes para el usuario
+        String success_msg = "El archivo fue escrito exitosamente.";
+        String write_error_msg = "Error al intentar escribir el archivo.";
+
         // Crear respuesta por defecto
         answer[0] = "true";
         answer[1] = "";
 
-        // Abrir archivo para escribir
+        // Intentar escribir el archivo
         try {
+            // Abrir el archivo para escritura
             file_writer = new FileWriter(current_file, StandardCharsets.UTF_8);
-        } catch (IOException io_exc) {
-            answer[0] = "false";
-            answer[1] = String.valueOf(io_exc.getCause());
-            throw new RuntimeException(io_exc);
-        } finally {
-            if (answer[0].equals("true")) {
-                file_buffer = new BufferedWriter(file_writer);
-                while (content_list.hasNext()) {
-                    file_buffer.write(content_list.next());
-                    file_buffer.newLine();
-                }
-                file_buffer.close();
+            file_buffer = new BufferedWriter(file_writer);
+
+            // Escribir los datos e insertar un salto de línea
+            while (content_list.hasNext()) {
+                file_buffer.write(content_list.next());
+                file_buffer.newLine();
             }
+
+            // Cerrar el archivo
+            file_buffer.close();
+
+        } catch (IOException io_exc) {
+            // Si se produce una excepción, preparar un mensaje de error
+            answer[0] = "false";
+            exc_cause = String.valueOf(io_exc.getCause());
+
+            // Guardar mensaje de error en el segundo elemento de la respuesta
+            answer[1] = "%s%s%s".formatted(write_error_msg, new_line, exc_cause);
         }
+
+        // Preparar mensaje de éxito para el usuario
+        answer[1] = answer[0].equals("true") ? success_msg : answer[1];
 
         // Retornar respuesta
         return answer;
     }
 
-    private static String[] readFile(String directory, String file_name) throws IOException {
+    private static String[] readFile(String directory, String file_name) {
         // Definir variables de trabajo y respuesta
-        String separator = File.separator;
-        String folder_path = "src%s%s".formatted(separator, directory);
+        String folder_path = "%s%s".formatted(src_directory, directory);
         String file_path = "%s%s%s".formatted(folder_path, separator, file_name);
         File current_file = new File(file_path);
         FileReader file_reader = null;
         BufferedReader file_buffer = null;
-        String line_read = "";
+        String new_datum = "";
         String[] answer = new String[3];
+
+        // Crear mensajes para el usuario
+        String success_msg = "El archivo fue leído exitosamente.";
+        String read_error_msg = "Error al intentar leer el archivo.";
 
         // Crear respuesta por defecto
         answer[0] = "true";
         answer[1] = "";
         answer[2] = "";
 
-        // Abrir archivo para leerlo
+        // Intentar leer el archivo
         try {
+            // Abrir el archivo
             file_reader = new FileReader(current_file, StandardCharsets.UTF_8);
-        } catch (IOException io_exc) {
-            answer[0] = "false";
-            answer[2] = String.valueOf(io_exc.getCause());
-            throw new RuntimeException(io_exc);
-        }
-        if (answer[0].equals("true")) {
             file_buffer = new BufferedReader(file_reader);
-            try {
-                line_read = file_buffer.readLine();
-            } catch (IOException io_exc) {
-                answer[0] = "false";
-                answer[2] = String.valueOf(io_exc.getCause());
-                throw new RuntimeException(io_exc);
-            }
-        }
-        if (answer[0].equals("true")) {
-            while (line_read != null) {
-                answer[1] = "%s%s%s".formatted(answer[1], System.lineSeparator(), line_read);
-                try {
-                    line_read = file_buffer.readLine();
-                } catch (IOException io_exc) {
-                answer[0] = "false";
-                answer[2] = String.valueOf(io_exc.getCause());
-                throw new RuntimeException(io_exc);
-                }
-            }
+
+            // Leer línea por línea
+            do {
+                new_datum = file_buffer.readLine();
+                answer[2] = "%s%s%s".formatted(answer[2], separator, new_datum);
+            } while (new_datum != null);
+
+            // Cerrar el archivo
             file_buffer.close();
+            file_reader.close();
+
+        } catch (IOException io_exc) {
+            // Si se produce una excepción, preparar un mensaje de error
+            answer[0] = "false";
+            exc_cause = String.valueOf(io_exc.getCause());
+            answer[1] = "%s.%s%s".formatted(read_error_msg, new_line, exc_cause);
         }
-        file_reader.close();
+
+        // Preparar mensaje de éxito para el usuario
+        answer[1] = answer[0].equals("true") ? success_msg : answer[1];
 
         // Retornar respuesta
         return answer;
@@ -158,69 +178,84 @@ public class LecturaEscritura {
         ArrayList<String> answer;
 
         // Separar cada elemento usando el salto de línea y guardarlos en 'answer'
-        answer =  new ArrayList<String>(Arrays.asList(content.split(System.lineSeparator())));
+        answer =  new ArrayList<String>(Arrays.asList(content.split(separator)));
 
         // Retornar la respuesta
         return answer;
     }
 
-    private static Integer buscarTexto(String nombreFichero, String texto) throws IOException {
+    private static String[] buscarTexto(String nombreFichero, String texto) {
         // Crear variables de trabajo y respuesta
         ArrayList<String> database = new ArrayList<>();
         String[] read_data;
-        Integer answer;
+        int repetitions;
+        String[] answer = new String[3];
+
+        // Crear mensaje de éxito para el usuario
+        String success_msg = "\nCantidad de repeticiones del texto: %s.\n";
 
         // Leer los datos desde el archivo especificado
         read_data = readFile("static", nombreFichero);
 
-        // Comprobar si el archivo pudo ser leído. Si falló, imprimir el mensaje de error y retornar
-        if (read_data[0].equals("false")) {
-            System.out.println(read_data[2]);
-            return -1;
+        // Comprobar si el archivo pudo ser leído.
+        if (read_data[0].equals("true")) {
+            // Si la lectura fue exitosa, convertir los datos en un ArrayList
+            database = stringToArray(read_data[2]);
+
+            // Contar el número de veces que se repite el texto buscado en la base de datos
+            repetitions = Collections.frequency(database, texto);
+
+            // Preparar la respuesta
+            answer[0] = "true";
+            answer[1] = "";
+            answer[2] = success_msg.formatted(repetitions);
         }
+        // Si falló, retornar el resultado de la lectura como respuesta
+        else answer = read_data;
 
-        // Caso contrario, convertir los datos en un ArrayList
-        database = stringToArray(read_data[1]);
-
-        // Contar el número de veces que se repite el texto buscado en la base de datos
-        answer = Collections.frequency(database, texto);
 
         // Retornar la respuesta
         return answer;
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         // Definir una variable para contener datos de ejemplo que serán grabados en un archivo
         ArrayList<String> dummy_data = new ArrayList<String>();
 
-        // Crear variables de trabajo y de respuesta
-        String answer;
+        // Crear un objeto Scanner para leer texto desde la consola
+        Scanner console = new Scanner(System.in, StandardCharsets.UTF_8);
+
+        // Crear otras variables de trabajo y la de respuesta
         String[] folder_file_creation = new String[2];
         String[] file_write_result = new String[2];
         String[] file_read_result = new String[3];
+        boolean valid_choice;
         ArrayList<String> data_array = new ArrayList<>();
+        String search_string;
+        String[] search_result;
 
-        // Definir los mensajes al usuario
-        String title_msg = "\n\nCalculando la suma y el promedio de una lista de números.\n" +
+        // Crear los mensajes para el usuario
+        String title_msg = "\n\nEscribiendo y Leyendo Archivos.\n" +
                 "--------------------------------------------------------\n\n";
-        String sum_msg_base = "Para la entrada anterior, el resultado de la suma es %d.";
-        String avg_msg_base = "Para la entrada anterior, el resultado del promedio es %.2f.";
-        String answer_msg = "\n\n%s\n%s\n\n";
+        String inst_1 = "\nPrimero, crearemos un archivo llamado 'listado.txt' dentro de la carpeta 'static'." +
+                "\nEsta operación tuvo el siguiente resultado:\n";
+        String inst_2 = "\nLuego, escribiremos en el archivo las palabras 'Perro', 'Gato', 'Juan', 'Daniel', y 'Camila'." +
+                "\nEsta operación tuvo el siguiente resultado:\n";
+        String inst_3 = "\nFinalmente, puedes intentar buscar una de esas palabras (u otras) y contar cuántas veces se repite.";
+        String inst_4 = "\nIngresa la palabra que quieres buscar: ";
+        String inst_5 = "\nEsta operación tuvo el siguiente resultado:\n";
+        String input_error_msg = "\nLa opción ingresada no es válida. Por favor, elige un número del menú.";
         String end_msg = "\n\nEl programa ha terminado. Adios.";
-        String output_msgs = "%s%s%s";
-        String read_success_msg ="\nEl archivo ha sido leído exitosamente.\n";
-        String read_error_msg = "\nError al intentar leer el archivo.\n";
-        String write_success_msg ="\nEl archivo ha sido escrito exitosamente.\n";
-        String write_error_msg = "\nError al intentar escribir en el archivo.\n";
+
+        // Imprimir título y primera instrucción
+        System.out.printf("%s%s", title_msg, inst_1);
 
         // Crear directorio 'static' y archivo 'listado.txt'
-        try {
-            folder_file_creation = crearArchivo("static", "listado.txt");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.printf("folder_file_creation = %s\n%s%n", folder_file_creation[0], folder_file_creation[1]);
+        folder_file_creation = crearArchivo("static", "listado.txt");
+
+        // Imprimir los mensajes de resultado de la operación
+        System.out.println(folder_file_creation[1]);
 
         // Crear datos de ejemplo para grabar en un archivo
         dummy_data.add("Perro");
@@ -234,20 +269,60 @@ public class LecturaEscritura {
         dummy_data.add("Daniel");
         dummy_data.add("Camila");
 
+        // Imprimir segunda instrucción
+        System.out.print(inst_2);
+
         // Grabar datos de ejemplo en un archivo
         file_write_result = writeBuffer("static", "listado.txt", dummy_data);
-        System.out.printf("file_write_result = %s\n%s\n", file_write_result[0], file_write_result[1]);
 
-        // Leer datos desde un archivo
+        // Imprimir los mensajes de resultado de la operación
+        System.out.println(file_write_result[1]);
+
+        // Leer datos desde el mismo archivo
         file_read_result = readFile("static", "listado.txt");
-        System.out.printf("file_read_result = %s\n%s\n%s\n", file_read_result[0], file_read_result[1], file_read_result[2]);
 
-        // Convertir el resultado de la lectura del archivo en un ArrayList
-        data_array = stringToArray(file_read_result[1]);
-        System.out.printf("data array = %s\n\n", data_array);
+        // Comprobar si la lectura fue exitosa
+        if (file_read_result[0].equals("true")) {
 
-        // Buscar cuántas veces se repite el nombre 'Camila' en los datos del archivo
-        Integer repetitions = buscarTexto("listado.txt", "Camila");
-        System.out.printf("El nombre 'Camila' se repite: %d veces en el archivo.\n", repetitions);
+            // Convertir el resultado de la lectura del archivo en un ArrayList
+            data_array = stringToArray(file_read_result[2]);
+
+            // Imprimir tercera y cuarta instrucciones
+            System.out.printf("%s%s", inst_3, inst_4);
+
+            // Leer la opción seleccionada desde la consola. Si el usuario ingresa un texto inválido,
+            // retornar un mensaje de error y volver a preguntar hasta que ingrese un número válido
+            search_string = console.next();
+            valid_choice = search_string.matches("^\\p{LD}+$");
+            if (!valid_choice) {
+                while (!valid_choice) {
+                    System.out.printf("%s%s", input_error_msg, inst_2);
+                    search_string = console.next();
+                    valid_choice = search_string.matches("^\\p{LD}+$");
+                }
+            }
+
+            // Buscar cuántas veces se repite el nombre 'Camila' en los datos del archivo
+            search_result = buscarTexto("listado.txt", search_string);
+
+            // Comprobar el resultado de la operación
+            if (search_result[0].equals("true")) {
+
+                // Si la búsqueda tuvo éxito imprimir el resultado
+                System.out.println(search_result[2]);
+            }
+            else {
+
+                // Si hubo un error, imprimir el mensaje de error de la búsqueda
+                System.out.println(search_result[1]);
+            }
+        }
+        else {
+            // Imprimir los mensajes de error de la lectura
+            System.out.println(file_read_result[1]);
+        }
+
+        // Imprimir el mensaje final y terminar el programa
+        System.out.println(end_msg);
     }
 }
